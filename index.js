8d8df25f0b5b6758d5df5b57f585418fe76695d9
Ken Kantzer <ken@pkcsecurity.com>
2019-09-20 16:39:12 -0700

    attempts to get images from blink and use rekog

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const _ = require('lodash');
+const getPictures = require('./blink')
+const {getCarCount} = require('./rekognition');
+
 module.exports.handler = async (event) => {
+  const pictures = await getPictures();
+  const carCount = await Promise.all(_.map(pictures, async (p) => await getCarCount(p)))
   const response = {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        carCount
       },
       null,
       2
