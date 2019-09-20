a85f38101dc541185763a2a3640506ad0b105018
alex joo <alex@pkcsecurity.com>
2019-09-20 16:48:46 -0700

    initial cut of react native app

---
+++ AbsenteeValet/geofence.js
@@ -0,0 +1,9 @@
+import { NativeModules } from 'react-native';
+
+const Geofence = NativeModules.Geofence;
+
+//GeoFence.setup = () => console.log('setup');
+
+//GeoFence.onEnter = (callback) => callback();
+
+export default GeoFence;
