a4e2de193dc9c2502fc3bf7558acc7f386b2944a
Jason Mellblom <jason@pkcsecurity.com>
2019-09-20 14:43:11 -0700

    working?

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const Blink = require('node-blink-security');
 var blink = new Blink(process.env.BLINK_USERNAME, process.env.BLINK_PASSWORD);
 blink.setupSystem().then(
   () => {
-    blink.setArmed().then(() => {
+    blink.getSummary().then((response) => {
       // see the object dump for details
+//console.log('response', response);
       console.log(blink);
     });
   },
