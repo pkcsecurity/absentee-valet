ceb45767854c4890cb8c5bebf4733cdd52236bfd
Jason Mellblom <jason@pkcsecurity.com>
2019-09-20 14:45:52 -0700

    new blink.js

---
+++ blink.js
@@ -0,0 +1,13 @@
+const blink = new Blink(process.env.BLINK_USERNAME, process.env.BLINK_PASSWORD);
+blink.setupSystem().then(
+  () => {
+    blink.getSummary().then((response) => {
+      // see the object dump for details
+//console.log('response', response);
+      console.log(blink);
+    });
+  },
+  error => {
+    console.log(error);
+  },
+);
