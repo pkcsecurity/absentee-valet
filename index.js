e4ba4d1e1aca6a260242f06c5b1ed0624aade610
alex joo <alex@pkcsecurity.com>
2019-09-20 12:15:29 -0700

    adds username and password

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Blink = require('node-blink-security');
 
-var blink = new Blink('', '');
-blink.setupSystem()
-  .then(() => {
-    blink.setArmed()
-      .then(() => {
-        // see the object dump for details
-        console.log(blink);
-      });
-  }, (error) => {
+var blink = new Blink(process.env.BLINK_USERNAME, process.env.BLINK_PASSWORD);
+blink.setupSystem().then(
+  () => {
+    blink.setArmed().then(() => {
+      // see the object dump for details
+      console.log(blink);
+    });
+  },
+  error => {
     console.log(error);
-  });
+  },
+);
